0fcc88ae32ee0dd56f52a26ebb15a27ec3ead8dc
root <root@ICB15.localdomain>
2017-12-17 17:19:18 -0800

    Version 1

--- queue.js
+++ queue.js
@@ -2,3 +2,15 @@ function Queue() {
 
   this.storage = [];
 };
+Queue.prototype.add = function(value){
+  this.storage.push(value);
+}
+Queue.prototype.remove = function(){
+  if(this.storage.length){
+    return this.storage.shift()
+    //we'll get the thing which we just dequeued
+  }
+}
+Queue.prototype.size = function(){
+ return this.storage.length;
+}
--- stack.js
+++ stack.js
@@ -1,5 +1,17 @@
 function Stack() {
 
   var storage = [];
+  Stack.add = function(value){
+  this.storage.push(value);
+  }
+  Stack.remove = function(){
+  if(this.storage.length){
+    return this.storage.shift()
+    //we'll get the thing which we just dequeued
+    }
+  }
+  Stack.size = function(){
+    return this.storage.length;
+  }
 
 }
